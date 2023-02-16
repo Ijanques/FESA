3df2a1d4575593db50d039277eac0b444dc328c1
Evandro Ijanques Junior <evandroijanquesjunior@gmail.com>
2023-02-16 20:41:36 -0300

    Exercise of creating a simple game registration process
    a simple console application that can take some inputs to register a game

---
+++ POO/POO ex01/POO ex01/Game.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex01
+{
+    internal class Game
+    {    
+        private int code;
+        private string name;
+        private string category;
+        private DateTime date;
+
+        public void SetCode(int cod)
+        {
+            if (cod <= 0)
+            {
+                throw new Exception("The game code can't be 0 or less");
+            }
+            else
+            {
+                code= cod;
+            }
+        }
+
+        public int GetCode() => code;
+
+        public void SetName(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new Exception("The game should have a valid name");
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+
+        public string GetName() => name;
+
+        public void SetCategory(string category) 
+        {
+            if(String.IsNullOrEmpty(category)) 
+            {
+                throw new Exception("The game should have a valid category name");
+            }
+            else
+            {
+                this.category= category;
+            }
+        }
+        public string GetCategory() => category;    
+
+        public void SetDate(DateTime date)
+        {
+            if (date == null)
+            {
+                throw new Exception("The game should have a valid release date");
+            }
+            else
+            {
+                this.date = date;
+            }
+        }
+        public DateTime GetDate() => date; 
+    }
+}
---
+++ POO/POO ex01/POO ex01/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter = "not y or n";
+            Console.WriteLine("Hi, this is an App to register games.\n\nPlease press any key to continue");
+            Console.ReadKey();
+            do
+            {
+                Console.Clear();
+                bool registerConfirmation = false;
+                Game game = new Game();
+
+                do
+                {
+                    do
+                    {
+                        
+                        try
+                        {
+                            Console.WriteLine("Type the code of the game: ");
+                            game.SetCode(Convert.ToInt32(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type the name of the game: ");
+                            game.SetName(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type the category of the game: ");
+                            game.SetCategory(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type the release date of the game: ");
+                            game.SetDate(Convert.ToDateTime(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    Console.WriteLine("Please confirm the data of the game:\n"
+                        + game.GetCode() + "\n" + game.GetName() + "\n" + game.GetCategory() +
+                        "\n" + game.GetDate());
+                    letter = "not y or n";
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type 'y' to confirm and register the game or 'n' to decline and start the registration process again");
+                            letter = Console.ReadLine().ToLower();
+                            registerConfirmation = true;
+                            Console.Clear();
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+
+                    } while (letter != "y" && letter != "n");
+                } while (!registerConfirmation);
+
+                Console.WriteLine("The game was succesfully registered");
+                letter = "not y or n";
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Type 'y' to start a new registration process or 'n' to decline and close the app");
+                        letter = Console.ReadLine().ToLower();
+                        registerConfirmation = true;
+                        Console.Clear();
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+
+                } while (letter != "y" && letter != "n");
+            } while (letter == "y");
+        }
+    }
+}
