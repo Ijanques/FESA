0b7a2bffbf4615075678a89358959b92097f87d2
Evandro Ijanques Junior <evandroijanquesjunior@gmail.com>
2023-02-16 22:27:31 -0300

    exercise of registering up to 10 games and then showing then
    in this exercise we had to register up to 10 games in the app and after that showing all the games in the same time

---
+++ POO/POO ex02/POO ex02/Game.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex02
+{
+    internal class Game
+    {
+        private int code;
+        private string name;
+        private string category;
+        private DateTime date;
+        private List<string> categorys = new List<string>() {"ação", "luta", "tiro", "esportes"};
+
+        public void SetCode(int cod)
+        {
+            if (cod <= 0)
+            {
+                throw new Exception("The game code can't be 0 or less");
+            }
+            else
+            {
+                code = cod;
+            }
+        }
+
+        public int GetCode() => code;
+
+        public void SetName(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                throw new Exception("The game should have a valid name");
+            }
+            else
+            {
+                this.name = name;
+            }
+        }
+
+        public string GetName() => name;
+
+        public void SetCategory(string category)
+        {
+            if (String.IsNullOrEmpty(category) || !categorys.Contains(category.ToLower()))
+            {
+                throw new Exception("The game should have a valid category name");
+            }
+            else
+            {
+                this.category = category;
+            }
+        }
+        public string GetCategory() => category;
+
+        public void SetDate(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                throw new Exception("The game should have a valid release date");
+            }
+            else
+            {
+                this.date = date;
+            }
+        }
+        public DateTime GetDate() => date;
+
+        public void GetInfo()
+        {
+            Console.WriteLine(code + "\n" + name + "\n" + category + "\n" + date);
+        }
+    }
+}
---
+++ POO/POO ex02/POO ex02/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string letter = "not y or n";
+            int count =0;
+            List<Game> games= new List<Game>();
+            Console.WriteLine("Hi, this is an App to register games (here you can register up to 10 games.\n\nPlease press any key to continue");
+            Console.ReadKey();
+            do
+            {
+                Console.Clear();
+                bool registerConfirmation = false;
+                Game game = new Game();
+
+                do
+                {
+                    do
+                    {
+
+                        try
+                        {
+                            Console.WriteLine("Type the code of the game: ");
+                            game.SetCode(Convert.ToInt32(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type the name of the game: ");
+                            game.SetName(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type the category of the game: ");
+                            game.SetCategory(Console.ReadLine());
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type the release date of the game: ");
+                            game.SetDate(Convert.ToDateTime(Console.ReadLine()));
+                            Console.Clear();
+                            break;
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+                    } while (true);
+                    Console.WriteLine("Please confirm the data of the game:\n");
+                    game.GetInfo();
+                    letter = "not y or n";
+                    do
+                    {
+                        try
+                        {
+                            Console.WriteLine("Type 'y' to confirm and register the game or 'n' to decline and start the registration process again");
+                            letter = Console.ReadLine().ToLower();
+                            registerConfirmation = true;
+                            Console.Clear();
+                        }
+                        catch (Exception error)
+                        {
+                            Console.WriteLine(error.Message);
+                        }
+
+                    } while (letter != "y" && letter != "n");
+                } while (!registerConfirmation);
+
+                games.Add(game);
+                Console.WriteLine("The game was succesfully registered");
+                letter = "not y or n";
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Type 'y' to start a new registration process or 'n' to decline and close the app");
+                        letter = Console.ReadLine().ToLower();
+                        registerConfirmation = true;
+                        Console.Clear();
+                    }
+                    catch (Exception error)
+                    {
+                        Console.WriteLine(error.Message);
+                    }
+
+                } while (letter != "y" && letter != "n");
+                count++;
+            } while (letter == "y" && count <10);
+            foreach(Game game in games)
+            {
+                Console.WriteLine("Informações do jogo " + game.GetName());
+                game.GetInfo();
+            }
+            Console.ReadKey();
+        }
+    }
+}
+    
