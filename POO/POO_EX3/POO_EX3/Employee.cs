990987b1e66c529e3088b224239d66d1d2500f05
Evandro Ijanques Junior <evandroijanquesjunior@gmail.com>
2023-02-23 22:42:37 -0300

    The beginning of the third exercise

--- POO/POO_EX3/POO_EX3/Employee.cs
+++ POO/POO_EX3/POO_EX3/Employee.cs
@@ -8,5 +8,41 @@ namespace POO_EX3
 {
     internal class Employee
     {
+        private int cod;
+        private string name;
+        private string rg;
+        private double wage;
+
+        public void SetCodigo(int value)
+        {
+            if (value <= 0)
+                throw new Exception("Please enter a valid code");
+            cod = value;
+        }
+        public int GetCodigo() => cod;
+
+        
+        
+        public void SetWage(double value)
+        {
+            if (value < 0)
+                throw new Exception("Please enter a valid wage");
+            wage = value; 
+        }
+        public double GetWage() => wage;
+        public void SetName(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new Exception("Please enter a valid name");
+            name = value;
+        }
+        public string GetName() => name;
+        public void SetRg(string value)
+        {
+            if (String.IsNullOrEmpty(value))
+                throw new Exception("Please enter a valid RG");
+            rg = value;
+        }
+        public string GetRg() => rg;
     }
 }
