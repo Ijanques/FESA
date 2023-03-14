f902316598a958997a2bb084e7153333dc895dda
Evandro Ijanques Junior <evandroijanquesjunior@gmail.com>
2023-03-14 20:56:07 -0300

    Exercise of overloading

---
+++ POO/POO_EX_07/POO_EX_07/Adder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_EX_07
+{
+    internal class Adder
+    {
+        public static int Add(int n1, int n2 )
+        {
+            return n1 + n2;
+        }
+        public static double Add(double n1, double n2)
+        { 
+            return n1 + n2; 
+        }
+        public static double Add(string n1, string n2)
+        {
+            return Convert.ToDouble(n1) + Convert.ToDouble(n2);
+        }
+    }
+}
---
+++ POO/POO_EX_07/POO_EX_07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_EX_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine(Adder.Add(1, 2));
+                Console.WriteLine(Adder.Add(1.90089, 4.567008));
+                Console.WriteLine(Adder.Add("2,567", "2,5"));
+            }catch(Exception error) 
+            {
+                Console.WriteLine(error.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
