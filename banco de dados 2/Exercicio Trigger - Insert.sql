0132b650cb92c71742cbf9c20fe53ad2543927a6
Evandro Ijanques Junior <evandroijanquesjunior@gmail.com>
2023-03-24 22:43:21 -0300

    Continuação dos exercicios de trigger

--- banco de dados 2/Exercicio Trigger - Insert.sql
+++ banco de dados 2/Exercicio Trigger - Insert.sql
@@ -15,6 +15,13 @@
 	for excluida o sistema deverá automaticamente excluir
 	todos os itens que existem para essa nota na tabela
 	de itensNotaFiscal
+
+	4)Criar uma trigger de inclusão baseada no resultado do exercicio 2 
+	que quando um produto for vendidm a quantidade que foi 
+	vendida deverá ser subtraida da quantidade em estoque do produto
+
+	5)Criar uma trigger de exclusão de itens de nota fiscal que,
+	quando um item de nota fiscal for exclido a quantidade do produto deverá retornar para o estoque
 */
 use Comercio
 --EX1 Trigger inserir nota fiscal
@@ -23,14 +30,14 @@ for insert as
 begin
 	declare @data smalldatetime
 	select @data = dataemissao from inserted
-	if @data < SYSDATETIME()
+	if @data <> GETDATE()
 	begin
 		raiserror('A data da nota fiscal não pode ser menor que a atual', 16, 1)
 		rollback transaction
 	end
 end
 
-insert into notafiscal values(1, '11-09-2023', 'William')
+insert into notafiscal values(3, '2023-03-25', 'William')
 
 
 --EX2 
@@ -57,8 +64,8 @@ begin
 		rollback transaction
 	end
 end
-insert into itensNotaFiscal values (5, 4, 4, 27.04)
-
+insert into itensNotaFiscal values (3, 4, 11, 27.04)
+select * from produto
 --ex3
 
 create or alter trigger trg_excluiNota on notafiscal
@@ -70,3 +77,30 @@ begin
 end
 
 delete from notaFiscal where nrNota = 5 
+
+--Ex4
+create or alter trigger trg_insereItens on itensnotafiscal
+for insert as 
+begin
+	declare @qtd int
+	declare @codProduto decimal(10,2)
+	declare @valor decimal(10,2)
+
+	select @qtd = quantidade, @codProduto = codproduto, @valor = valor from inserted
+	
+
+
+	if @qtd < 0 or (@qtd > (select qtdestoque from produto where codProduto = @codProduto))
+	begin
+		raiserror('A quantidade não pode ser 0 ou menor', 14, 1)
+		rollback transaction
+	end
+	if @valor <> ((select preco from produto where codProduto = @codProduto) * @qtd)
+	begin 
+		raiserror('O valor deve ser inserido corretamente', 14, 1)
+		rollback transaction
+	end
+	update produto
+	set qtdEstoque = qtdEstoque - @qtd
+	where codProduto = @codProduto
+end
