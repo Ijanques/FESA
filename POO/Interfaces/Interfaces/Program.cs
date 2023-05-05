729c95ce4800400c543f04c6bcbd8c6c3e3d6bb6
Evandro Ijanques Junior <evandroijanquesjunior@gmail.com>
2023-05-04 22:30:31 -0300

    Estudo sobre a aplicação de interfaces em c#
    Programa que teve como objetivo o aprendizado sobre a utlização das interfaces em c#

---
+++ POO/Interfaces/Interfaces/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarroDePasseio carro = new CarroDePasseio();
+            carro.Placa = "KPC-1234";
+            carro.Acelerar();
+            carro.Acelerar();
+            carro.IndicarDirecao(EnumDirecao.Esquerda);
+            Console.WriteLine(carro.VelocidadeAtual);
+            Moto moto= new Moto();
+            moto.Placa = "YIM-2101";
+            moto.Acelerar();
+            moto.Acelerar();
+            moto.Acelerar();
+            moto.Buzinar();
+            moto.IndicarDirecao(EnumDirecao.Direita);
+            Console.WriteLine(moto.VelocidadeAtual);
+            
+
+            //foreach para mostrar a placa dos veiculos
+
+           List<Iveiculo> veiculos = new List<Iveiculo>();
+            veiculos.Add(moto);
+            veiculos.Add(carro);
+            foreach(Iveiculo i in veiculos) 
+            {
+                Console.WriteLine(i.Placa);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
